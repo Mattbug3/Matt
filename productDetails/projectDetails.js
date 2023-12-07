54881a7f55b1dabda6d3feca52bb26c8c5febbd8
Mattbug3 <120091253+Mattbug3@users.noreply.github.com>
2023-12-07 15:46:20 +0800

    projectDetails.js

--- productDetails/projectDetails.js
+++ productDetails/projectDetails.js
@@ -7,35 +7,35 @@ const closeIcon_2 = document.querySelector('.close-icon-2')
 const menuIcon_3 = document.querySelector('.menu-icon-3')
 const closeIcon_3 = document.querySelector('.close-icon-3')
 
-menuIcon_1.addEventListener('click', () => {
-    listContainer.style.right='0px'
-    menuIcon.style.display = 'none'
+document.querySelector('.menu-icon-1').addEventListener('click', () => {
+    document.querySelector('.list-container').style.right='0px'
+    document.querySelector('.menu-icon').style.display = 'none'
 })
 
 
-closeIcon_1.addEventListener('click', () => {
-    listContainer.style.right='-300px'
-    menuIcon.style.display = 'block'
+document.querySelector('.close-icon-1').addEventListener('click', () => {
+    document.querySelector('.list-container').style.right='-300px'
+    document.querySelector('.menu-icon').style.display = 'block'
 })
 
-menuIcon_2.addEventListener('click', () => {
-    listContainer.style.right='0px'
-    menuIcon.style.display = 'none'
-})
-
-
-closeIcon_2.addEventListener('click', () => {
-    listContainer.style.right='-300px'
-    menuIcon.style.display = 'block'
-})
-
-menuIcon_3.addEventListener('click', () => {
-    listContainer.style.right='0px'
-    menuIcon.style.display = 'none'
+document.querySelector('.menu-icon-2').addEventListener('click', () => {
+    document.querySelector('.list-container').style.right='0px'
+    document.querySelector('.menu-icon').style.display = 'none'
 })
 
 
-closeIcon_3.addEventListener('click', () => {
-    listContainer.style.right='-300px'
-    menuIcon.style.display = 'block'
+document.querySelector('.close-icon-2').addEventListener('click', () => {
+    document.querySelector('.list-container').style.right='-300px'
+    document.querySelector('.menu-icon').style.display = 'block'
+})
+
+document.querySelector('.menu-icon-3').addEventListener('click', () => {
+    document.querySelector('.list-container')r.style.right='0px'
+    document.querySelector('.menu-icon').style.display = 'none'
+})
+
+
+document.querySelector('.close-icon-3').addEventListener('click', () => {
+    document.querySelector('.list-container').style.right='-300px'
+    document.querySelector('.menu-icon').style.display = 'block'
 })
